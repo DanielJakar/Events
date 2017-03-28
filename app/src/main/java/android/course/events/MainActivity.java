7330bc19e2667dc921f855c86c490aad386f1322
DanielJakar <danieljakar@gmail.com>
2017-03-28 20:19:51 +0300

    Counter with plus/minus

--- app/src/main/java/android/course/events/MainActivity.java
+++ app/src/main/java/android/course/events/MainActivity.java
@@ -5,28 +5,59 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // define Proerties:
     //in activities / Controllers the properties are the controls.
-    private EditText etMessage;
-    private Button btnSend;
+
+    private TextView tvResult;
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSend = (Button) findViewById(R.id.btnSend); //fbc
-        etMessage = (EditText) findViewById(R.id.etMessage);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        setTvResult();
+
+
+
+
+
+//        btnSend = (Button) findViewById(R.id.btnSend); //fbc
+//        etMessage = (EditText) findViewById(R.id.etMessage);
+//
+//
+//    }
+//
+//    public void btnClicked(View view) {
+//        String text = etMessage.getText().toString();
+//        Toast.makeText(this, "Hello " + text, Toast.LENGTH_SHORT).show();
+//
+    }
+
+    private void setTvResult(){
+        tvResult.setText(String.valueOf(counter));
+    }
+
+    public void btnClick2(View view) {
+        //btn Plus
+        counter++;
+        setTvResult();
+
 
 
     }
 
     public void btnClicked(View view) {
-        String text = etMessage.getText().toString();
-        Toast.makeText(this, "Hello " + text, Toast.LENGTH_SHORT).show();
+        //btn Minus
+        counter--;
+        setTvResult();
     }
 }
